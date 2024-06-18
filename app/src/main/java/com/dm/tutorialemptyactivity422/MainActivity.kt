f02cb0a722eebcc659bb81ffc1bf4508eebc2f6c
dinhductri202com <anous12bg@gmail.com>
2024-06-18 13:58:59 +0700

    Update MainActivity.kt

--- app/src/main/java/com/dm/tutorialemptyactivity422/MainActivity.kt
+++ app/src/main/java/com/dm/tutorialemptyactivity422/MainActivity.kt
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+   
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val myWebView: WebView = findViewById(R.id.webview)
+        myWebView.loadUrl("http://www.google.com")
     }
 }
